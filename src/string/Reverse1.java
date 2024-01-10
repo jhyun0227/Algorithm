d38b066c801bb206ed59786fbbf23e8b63701e31
junho <jhyun0227@uniess.co.kr>
2024-01-11 08:56:47 +0900

    문자배열 뒤집기 연습

---
+++ src/string/Reverse1.java
@@ -0,0 +1,32 @@
+package string;
+
+public class Reverse1 {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+
+        char[] array = {'d', 'a', '2', 'c', 'a', 'r'};
+        reverse(array);
+
+        long end = System.nanoTime();
+
+        System.out.println("time = " + (end - start));
+    }
+
+    public static void reverse(char[] array) {
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            char temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        for (char c : array) {
+            System.out.println(c);
+        }
+    }
+}
