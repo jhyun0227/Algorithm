7d4a7553a4eea967fc04c092234d22e9a95335a8
junho <jhyun0227@uniess.co.kr>
2024-01-16 09:22:29 +0900

    가장 긴 팰린드롬 수정

--- src/string/LongestPalindrome1.java
+++ src/string/LongestPalindrome1.java
@@ -9,15 +9,18 @@ public class LongestPalindrome1 {
             return input;
         }
 
+        //글자가 짝수일경우, 홀수일 경우를 가정하여 2포인터, 3포인터를 사용
         for (int i = 0; i < input.length() - 1; i++) {
             extendPalindrome(input, i, i+1);
             extendPalindrome(input, i, i+2);
         }
 
-        return input.substring(left, maxLength);
+        //시작점 + 최대글자수
+        return input.substring(left, left + maxLength);
     }
 
     private void extendPalindrome(String input, int i, int j) {
+        //i, j가 범위에 맞고, 해당 위치의 문자가 같은 문자일 경우
         while (i >= 0 && j < input.length() && input.charAt(i) == input.charAt(j)) {
             i--;
             j++;
--- src/string/LongestPalindromeMain.java
+++ src/string/LongestPalindromeMain.java
@@ -7,7 +7,8 @@ public class LongestPalindromeMain {
 
         long start = System.nanoTime();
 
-        String input = "dcbabcdd";
+//        String input = "dcbabcdd";
+        String input = "cdabbadf";
         System.out.println(longestPalindrome1.longestPalindrome(input));
 
         long end = System.nanoTime();
