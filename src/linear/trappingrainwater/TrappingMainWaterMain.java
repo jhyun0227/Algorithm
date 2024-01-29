f8f8488237014ce7bb97153f1d5d7ee1ef3a0f4f
junho <jhyun0227@uniess.co.kr>
2024-01-30 08:40:53 +0900

    fix. 빗물, Stack 이용해서 구하기.. 이것보다 투포인터가 나을것 같다 더 직관적

--- src/linear/trappingrainwater/TrappingMainWaterMain.java
+++ src/linear/trappingrainwater/TrappingMainWaterMain.java
@@ -4,12 +4,13 @@ import linear.twosum.TwoSum4;
 
 public class TrappingMainWaterMain {
     public static void main(String[] args) {
-        TrappingRainWater1 trappingRainWater1 = new TrappingRainWater1();
+//        TrappingRainWater1 trappingRainWater = new TrappingRainWater1();
+        TrappingRainWater2 trappingRainWater = new TrappingRainWater2();
 
         long start = System.nanoTime();
 
         int[] height = {1, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        trappingRainWater1.trappingRainWater(height);
+        trappingRainWater.trappingRainWater(height);
 
         long end = System.nanoTime();
 
---
+++ src/linear/trappingrainwater/TrappingRainWater2.java
@@ -0,0 +1,31 @@
+package linear.trappingrainwater;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TrappingRainWater2 {
+
+    public void trappingRainWater(int[] height) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int volume = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                Integer top =  stack.pop();
+
+                if (stack.isEmpty()) {
+                    break;
+                }
+
+                int distance = i - stack.peek() - 1;
+                int waters = Math.min(height[i], height[stack.peek()]) - height[top];
+
+                volume += distance * waters;
+            }
+
+            stack.push(i);
+        }
+
+        System.out.println("volume = " + volume);
+    }
+}
