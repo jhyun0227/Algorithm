d82379ac779fc08b811df74526dae9c1f2a4d67c
junho <jhyun0227@uniess.co.kr>
2024-02-05 09:40:28 +0900

    백준 10813

--- src/baekjoon/bj10813.java
+++ src/baekjoon/bj10813.java
@@ -1,7 +1,8 @@
-import java.io.*;
-import java.util.Arrays;
+package baekjoon;
 
-public class Main {
+import java.io.*;
+
+public class bj10813 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
